66474b3697ebd19cf309cedbf9d5c4c3d060ae79
vedirereddy <vedirereddy1@gmail.com>
2026-09-15 09:45:56 +0000

    Fix frontend eslint rules

--- starter/frontend/.eslintrc.js
+++ starter/frontend/.eslintrc.js
@@ -1,38 +1,25 @@
 module.exports = {
-    ignorePatterns: ['build/', 'dist/', 'node_modules/'],
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
-    plugins: ['react', 'prettier'],
-    settings: {
-      react: {
-        version: 'detect',
-      },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-    env: {
-      browser: true,
-      es6: true,
-      node: true,
-      jest: true,
-    },
-    rules: {
-      'prettier/prettier': [
-        'error',
-        {
-          singleQuote: true,
-          jsxSingleQuote: false,
-          printWidth: 120,
-          parser: 'babel-ts',
-        },
-      ],
-      // Customize your rules here
-      'react/prop-types': process.env.FAIL_LINT ? 2 : 0,
-      'react/jsx-uses-vars': 'warn',
-    },
-  };
-  
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+  },
+};
